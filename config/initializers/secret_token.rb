253f64a57c28e8c7897d5063397cf628d4f82da2
valinaga <vali@experiment.ro>
2011-12-04 11:13:14 +0200

    ok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zuri::Application.config.secret_token = '562b61b7b3b70ae65e48bd0c6075e697430cb6efc30f5dc32a19cff7bd276aa884bdf4de938db39ce036d4403c201850e8e7665c995ec823e6eee1a5b950f3f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Zuri::Application.config.session_store :cookie_store, :key => '_Zuri_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Zuri::Application.config.session_store :active_record_store
